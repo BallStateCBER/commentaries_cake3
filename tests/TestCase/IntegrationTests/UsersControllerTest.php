9991353694a710f9547779f0b1ace1c06c8af4dd
Erica Dee Fox <ericadeefox@gmail.com>
2017-12-07 17:33:10 -0500

    Fix non-empty string error in phpunit

--- tests/TestCase/IntegrationTests/UsersControllerTest.php
+++ tests/TestCase/IntegrationTests/UsersControllerTest.php
@@ -44,6 +44,6 @@ class UsersControllerTest extends ApplicationTest
         ];
 
         $this->post('/login', $data);
-        $this->assertRedirect();
+        $this->assertRedirect('/');
     }
 }
