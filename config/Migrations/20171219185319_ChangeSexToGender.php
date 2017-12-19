e421a84a3d2358c43329a01fa931a591a98792c6
Erica Dee Fox <ericadeefox@gmail.com>
2017-12-19 14:05:16 -0500

    Write migration for sex to gender field

---
+++ config/Migrations/20171219185319_ChangeSexToGender.php
@@ -0,0 +1,22 @@
+<?php
+use Migrations\AbstractMigration;
+
+class ChangeSexToGender extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * More information on this method is available here:
+     * http://docs.phinx.org/en/latest/migrations.html#the-change-method
+     * @return void
+     */
+    public function change()
+    {
+        $table = $this->table('users');
+        $table->changeColumn('sex', 'string', ['limit' => 45])
+            ->renameColumn('sex', 'gender')
+            ->save();
+
+        $this->execute('UPDATE users SET gender = replace(gender, "m", "male"), gender = replace(gender, "f", "female"), gender = replace(gender, "F", "female")');
+    }
+}
