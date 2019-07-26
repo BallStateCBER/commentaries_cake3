991514696c218510f33162055c35486bc0e68a6d
Graham Watson <graham@phantomwatson.com>
2019-07-26 16:15:53 -0400

    Remove redundant loading of Debug plugin

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -244,14 +244,6 @@ Type::build('timestamp')
  * Inflector::rules('transliteration', ['/å/' => 'aa']);
  */
 
-/*
- * Only try to load DebugKit in development mode
- * Debug Kit should not be installed on a production system
- */
-if (Configure::read('debug')) {
-    Plugin::load('DebugKit', ['bootstrap' => true]);
-}
-
 /**
  * Connect middleware/dispatcher filters.
  */
