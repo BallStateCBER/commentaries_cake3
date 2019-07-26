98845df14b0ac8f60d8d4c5861be9304aa9678bf
Graham Watson <graham@phantomwatson.com>
2019-07-26 16:14:50 -0400

    Fix how email transport configuration is set

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -64,6 +64,7 @@ use Cake\Datasource\ConnectionManager;
 use Cake\Error\ErrorHandler;
 use Cake\Log\Log;
 use Cake\Mailer\Email;
+use Cake\Mailer\TransportFactory;
 use Cake\Network\Request;
 use Cake\Routing\DispatcherFactory;
 use Cake\Utility\Security;
@@ -167,7 +168,7 @@ if (!Configure::read('App.fullBaseUrl')) {
 
 Cache::setConfig(Configure::consume('Cache'));
 ConnectionManager::setConfig(Configure::consume('Datasources'));
-Email::setConfigTransport(Configure::consume('EmailTransport'));
+TransportFactory::setConfig(Configure::consume('EmailTransport'));
 Email::setConfig(Configure::consume('Email'));
 Log::setConfig(Configure::consume('Log'));
 Configure::write('security_salt', env('SECURITY_SALT'));
